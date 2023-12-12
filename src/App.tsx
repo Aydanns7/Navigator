24fa76a48d3dd40c1dadf969c4e3a25274163c8b
Aydanns7 <132332588+Aydanns7@users.noreply.github.com>
2023-12-12 15:00:50 +0400

    api

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,62 @@
-import { NavigationContainer } from "@react-navigation/native"
-import { createStackNavigator } from "@react-navigation/stack"
-import React from "react"
-import { DrawerNavigator } from "../navigation/Drawer"
-import { StackNavigator } from "../navigation/Stack"
+import React, { useEffect, useState } from "react";
+import { View, Text, TouchableOpacity, Image, TextInput, ScrollView } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
-export default function App() {
-    return(
-        <NavigationContainer>
-            <StackNavigator/>
-        </NavigationContainer>
-    )
+function App() {
+
+
+    const [movies, setMovies] = useState([]);
+
+
+    useEffect(() => {
+        getMovieData();
+    }, []);
+
+    const [searchMovie, setSearchMovie] = useState('');
+
+    const handleMovieName = () => {
+        getMovieData()
+    };
+
+    const getMovieData = async () => {
+        const response = await fetch(`https://api.tvmaze.com/search/shows?q=${searchMovie}`);
+        const respond = await response.json();
+        setMovies(respond);
+    }
+
+
+    return (
+        <SafeAreaView style={{ backgroundColor: "#F19CBB", flex:1}}>
+            <View style={{ flexDirection: "row", marginTop: 40 }}>
+
+                <TextInput
+                    placeholder='Write a movie name,please...'
+                    style={{width: 300, height: 50, borderRadius: 10, borderWidth: 2, backgroundColor: '#FEFEFA', marginLeft: 30, fontSize: 18 }}
+                    value={searchMovie}
+                    onChangeText={(text) => setSearchMovie(text)} />
+
+                <View style={{ flexDirection: 'column' }}>
+                    <TouchableOpacity
+                        onPress={handleMovieName}
+                        style={{ width: 50, height: 50, backgroundColor: '#8e4585', borderRadius: 10, borderWidth: 2, justifyContent: 'center', alignItems: 'center' }}>
+                        <Image style={{ width: 30, height: 30, alignItems: 'center', justifyContent: 'center' }} source={require('./assets/icons/Search.png')} />
+                    </TouchableOpacity>
+    </View>
+
+            </View>
+                    <ScrollView contentContainerStyle={{ flexGrow: 1}}>
+                        {movies.map((item, index) => (
+                                <View style={{justifyContent:"center",alignItems:"center",marginVertical:30}} key={index}>
+                                    <Text style={{justifyContent:'center', alignItems:'center', fontWeight:'bold', fontSize:20,color:"black"}}>{item.show.name}</Text>
+                                    <Image style={{ width: 70, height: 70, borderRadius:10}} source={{ uri: item.show.image.medium }} />
+                                </View>
+                        ))}
+                    </ScrollView>
+            
+        </SafeAreaView>)
 }
+
+
+
+export default App
 
